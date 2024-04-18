c3f6f0173352447d97d4ab18f8a02a36e1a3d1f9
bieben <yitezheng13@gmail.com>
2024-04-18 02:03:43 -0400

    revise app.js

--- src/App.js
+++ src/App.js
@@ -1,101 +1,47 @@
 import './App.css';
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Checkout from './components/Checkout/Checkout';
-import Orders from './components/Orders/Orders';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from './components/Login/Login';
-import Payment from './components/Payment/Payment';
 import { useEffect } from 'react';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
-import { loadStripe } from "@stripe/stripe-js"
-import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Checkout from './components/Checkout/Checkout';
+import Orders from './components/Orders/Orders';
+import Login from './components/Login/Login';
+import Payment from './components/Payment/Payment';
 
 const promise = loadStripe('pk_test_51NxlGQGVkWt1AECJwqYMRAs9PR5uXZl8o3JmPUCQmoaLaxuJX0Ff1Hdnw7YXansctp7xrvvRq4TFAqwAlbgnKk4N009ln75gDs');
 
 function App() {
-  const [{ }, dispatch] = useStateValue();
+  const [, dispatch] = useStateValue();
 
-  useEffect(() => { // like if statement in React
-    // will only run once when the app component loads...
+  useEffect(() => {
     auth.onAuthStateChanged(authUser => {
       console.log('THE USER IS >>> ', authUser);
-
-      if (authUser) {
-        // the user just logged in / the user was logged in
-        dispatch({
-          type: 'SET_USER',
-          user: authUser,
-        })
-      } else {
-        // the user is logged out
-        dispatch({
-          type: 'SET_USER',
-          user: null,
-        });
-      }
+      dispatch({
+        type: 'SET_USER',
+        user: authUser ? authUser : null,
+      });
     });
-  }, [])
+  }, []);
 
   return (
     <Router>
       <div className="app">
+        <Header />
         <Routes>
-          <Route
-            path='/orders'
-            element={
-              <div>
-                <Header />
-                <Orders />
-              </div>
-            } />
-        </Routes>
-
-        <Routes>
-          <Route
-            path='/login'
-            element={
-              <div>
-                <Header />
-                <Login />
-              </div>
-            } />
-        </Routes>
-
-        <Routes>
-          <Route
-            path='/checkout'
-            element={
-              <div>
-                <Header />
-                <Checkout />
-              </div>
-            } />
-        </Routes>
-
-        <Routes>
-          <Route
-            path='/payment'
-            element={
-              <div>
-                <Header />
-                <Elements stripe={promise}>
-                  <Payment />
-                </Elements>
-              </div>
-            } />
-        </Routes>
-        
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <div>
-                <Header />
-                <Home />
-              </div>
-            } />
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/orders' element={<Orders />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/payment' element={
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          } />
         </Routes>
       </div>
     </Router>
